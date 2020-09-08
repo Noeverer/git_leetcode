6d7cc8a909d170a8f508a6edcc540cddedf0eafe
Noeverer <robotliu0327@gmail.com>
2020-09-08 10:30:16 +0800

    更改文件名

--- code_leet/back_tracking/77.组合.py
+++ code_leet/back_tracking/77.组合.py
@@ -2,9 +2,34 @@
 #coding:utf-8
 
 """
-@author: Ante Liu
-@contact: robotliu0327@gmail.com
-@software: PyCharm
-@file: 77.组合.py
-@time: 2020-09-08 上午 10:11
+给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+
+示例:
+
+输入: n = 4, k = 2
+输出:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combinations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+import itertools
+class Solution:
+    def combine( n: int, k: int):
+        '''
+        使用python函数库的方式
+        :param k:
+        :return:
+        '''
+        return list(itertools.combinations(list(range(1,n+1)),k))
+
+
+
